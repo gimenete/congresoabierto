0ffa1abecbf339f96dab80c54a8dc5e883a15b2c
Alberto Gimeno <gimenete@gmail.com>
2011-05-08 12:04:11 +0200

    Better import.js

--- SessionParser/SessionParser/import.js
+++ SessionParser/SessionParser/import.js
@@ -23,62 +23,65 @@ function join(arr, prefix, callback) {
 	});
 }
 
-function searchDiputado(normalizado, callback) {
-	redis.zrange('diputados', 0, -1, function(err, ids) {
-		join(ids, 'diputado:', function(diputados) {
-			for (var i=0; i < diputados.length; i++) {
-				if(diputados[i].nombrenormalizado.indexOf(normalizado, 0) !== -1) {
-					callback(diputados[i])
-					return
-				}
-			}
-			callback(null)
-		})
-	})
-	
+function searchDiputado(normalizado) {
+	for (var i=0; i < diputados.length; i++) {
+		if(diputados[i].nombrenormalizado.indexOf(normalizado, 0) !== -1) {
+			return diputados[i]
+		}
+	}
 }
 
 function saveDiputado(normalizado, info, callback) {
-	searchDiputado(normalizado, function(diputado) {
-		if (diputado) {
-			console.log(diputado)
-			
-			var multi = redis.multi()
-				
-			for(key in info) {
-				multi.hset('diputado:'+diputado.id, key, info[key])
-			}
-			
-			multi.exec(function(err, response) {
-				callback(null, null)
-			})
-		} else {
-			console.log('not found')
-			callback(null, null)
-		}
-	})
-}
-
-async.series([
-	function(callback) {
-		var info = {
-			intervenciones: 20,
-			puntuacion: 200
+	var diputado = searchDiputado(normalizado)
+	if (diputado) {
+		console.log(diputado)
+		
+		var multi = redis.multi()
+		multi.zadd('diputados', info.puntuacion, diputado.id)
+		
+		for(key in info) {
+			multi.hset('diputado:'+diputado.id, key, info[key])
 		}
 		
-		saveDiputado('caldera-sanchez-capitan', info, callback)
-	},
-	
-	function foo(callback) {
-		console.log('foo')
+		multi.exec(function(err, response) {
+			callback(null, null)
+		})
+	} else {
+		console.log('not found: '+normalizado)
 		callback(null, null)
 	}
+}
 
-], 
-function(err, results) {
-	console.log('finished')
-	redis.quit();
-});
+
+var diputados
+
+redis.zrange('diputados', 0, -1, function(err, ids) {
+	join(ids, 'diputado:', function(_diputados) {
+		diputados = _diputados
+		
+		async.series([
+			function(callback) {
+				var info = {
+					intervenciones: 20,
+					puntuacion: 200
+				}
+
+				saveDiputado('caldera-sanchez-capitan', info, callback)
+			},
+
+			function foo(callback) {
+				console.log('foo')
+				callback(null, null)
+			}
+
+		], 
+		function(err, results) {
+			console.log('finished')
+			redis.quit();
+		});
+		
+	})
+})
 
 
 
