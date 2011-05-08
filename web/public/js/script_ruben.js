419dc163d50e8c1f572254f1972c8ef0d1d9eb02
rubenbp <rubenbp@gmail.com>
2011-05-08 20:11:26 +0200

    improve punch animation

--- web/public/js/script_ruben.js
+++ web/public/js/script_ruben.js
@@ -1,50 +1,47 @@
-function punch1(){
-	punch("punch1", "200px", "100px");
+function punch1(callback){
+	punch("punch1", "200px", "100px", callback);
 }
 
-function punch2(){
-	punch("punch2", "220px", "220px");	
+function punch2(callback){
+	punch("punch2", "220px", "220px", callback);	
 }
 
-function punch3(){
-	punch("punch3", "240px", "340px");
+function punch3(callback){
+	punch("punch3", "240px", "340px", callback);
 }
 
-function punch(class, start_top, end_top){
+function punch(class, start_top, end_top, callback){
 	var punch = $("<img src='/img/punch.gif' class='" + class + "'/>");
 	punch.appendTo('#fighter0');
 	punch.css("left","200px").css("top",start_top).css("opacity","0.0");
 	punch.animate({opacity: 1}, {queue: false, duration: 200})
 		 .animate({left: '700px', top: end_top}, {queue: false, duration: 600})
 		 .animate({opacity: 1}, 300)
-		 .animate({opacity: 0.0}, 100);
-}
-
-function multi_punch1(){
-	punch1(); 
-	punch2();
-	punch3();
-	$("#fighter1 .avatar").jrumble({ rumbleEvent: 'constant'});
+		 .animate({opacity: 0.0}, 100, callback);
 }
 
 function punchAttact(){
 	var punch = $("<img src='/img/punch.gif' class='punch1'/>");
-	//var punch1 = $("<img src='/img/punch.gif' class='punch2'/>");
-	//var punch2 = $("<img src='/img/punch.gif' class='punch3'/>");
 	
 	punch.appendTo('#fighter0');
-	punch.css("left","150px").css("top","200px").css("opacity","0.0");
-	punch.animate({opacity: 1}, 50)
-		 .animate({left: '700px', top: "100px"}, 400)
-		 .animate({opacity: 0.0}, 50)
+	punch.css("left","150px").css("top","200px");
+	punch.animate({left: '600px', top: "100px"}, 400)
 		 
 		 .animate({left: "150px", top: "220px"}, 1)
-		 .animate({opacity: 1}, 50)
-		 .animate({left: '700px', top: "220px"}, 400)
-		 .animate({opacity: 0.0}, 50)
+		 .animate({left: '600px', top: "220px"}, 400)
 		 
 		 .animate({left: "150px", top: "240px"}, 1)
-		 .animate({opacity: 1}, 50)
-		 .animate({left: '700px', top: "340px"}, 400)
-		 .animate({opacity: 0.0}, 50)
+		 .animate({left: '600px', top: "340px"}, 400, multiplePunchAttact);
 }
+
+function multiplePunchAttact(){
+	punch1(empty);
+	punch2(empty);
+	punch3(rumbleLosser);
+}
+
+function rumbleLosser(){
+	$("#fighter1 .avatar img").jrumble({ rumbleEvent: 'constant'});
+}
+
+function empty(){}
