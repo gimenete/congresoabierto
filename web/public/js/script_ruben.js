41db067a57ec5296a53baa615c4b44c1c6899554
rubenbp <rubenbp@gmail.com>
2011-05-08 20:41:18 +0200

    ended punch animation

--- web/public/js/script_ruben.js
+++ web/public/js/script_ruben.js
@@ -1,47 +1,74 @@
-function punch1(callback){
-	punch("punch1", "200px", "100px", callback);
+var fight_punchStart;
+var fight_punchEnd;
+var fight_style_sufix;
+
+function punchAttackLeftToRight(){
+	fight_punchStart = "150px";
+	fight_punchEnd = "600px";
+	fight_style_sufix = "_right";
+	punchAttact();
 }
 
-function punch2(callback){
-	punch("punch2", "220px", "220px", callback);	
-}
-
-function punch3(callback){
-	punch("punch3", "240px", "340px", callback);
-}
-
-function punch(class, start_top, end_top, callback){
-	var punch = $("<img src='/img/punch.gif' class='" + class + "'/>");
-	punch.appendTo('#fighter0');
-	punch.css("left","200px").css("top",start_top).css("opacity","0.0");
-	punch.animate({opacity: 1}, {queue: false, duration: 200})
-		 .animate({left: '700px', top: end_top}, {queue: false, duration: 600})
-		 .animate({opacity: 1}, 300)
-		 .animate({opacity: 0.0}, 100, callback);
+function punchAttackRightToLeft(){
+	fight_punchStart = "600px";
+	fight_punchEnd = "150px";
+	fight_style_sufix = "_left";
+	punchAttact();
 }
 
 function punchAttact(){
-	var punch = $("<img src='/img/punch.gif' class='punch1'/>");
+	
+	var punch = $("<img src='/img/punch.gif' class='punch" + fight_style_sufix + "'/>");
 	
 	punch.appendTo('#fighter0');
-	punch.css("left","150px").css("top","200px");
-	punch.animate({left: '600px', top: "100px"}, 400)
+	punch.css("left",fight_punchStart).css("top","200px");
+	punch.animate({left: fight_punchEnd, top: "100px"}, 400)
 		 
-		 .animate({left: "150px", top: "220px"}, 1)
-		 .animate({left: '600px', top: "220px"}, 400)
+		 .animate({left: fight_punchStart, top: "220px"}, 1)
+		 .animate({left: fight_punchEnd, top: "220px"}, 400)
 		 
-		 .animate({left: "150px", top: "240px"}, 1)
-		 .animate({left: '600px', top: "340px"}, 400, multiplePunchAttact);
+		 .animate({left: fight_punchStart, top: "240px"}, 1)
+		 .animate({left: fight_punchEnd, top: "340px"}, 400)
+		 .animate({opacity: 0.0},1, multiplePunchAttact);
 }
 
+var multiplePunchAttacks = 0;
 function multiplePunchAttact(){
+	multiplePunchAttacks++;
+	
 	punch1(empty);
 	punch2(empty);
-	punch3(rumbleLosser);
+	
+	if (multiplePunchAttacks <= 3)
+		punch3(multiplePunchAttact);
+	else 
+		punch3(rumbleLosser);	
 }
 
 function rumbleLosser(){
-	$("#fighter1 .avatar img").jrumble({ rumbleEvent: 'constant'});
+	fight_loser.jrumble({ rumbleEvent: 'constant'});
 }
 
 function empty(){}
+
+function punch1(callback){
+	punch("200px", "100px", callback);
+}
+
+function punch2(callback){
+	punch("220px", "220px", callback);	
+}
+
+function punch3(callback){
+	punch("240px", "340px", callback);
+}
+
+function punch(start_top, end_top, callback){
+	var punch = $("<img src='/img/punch.gif' class='punch" + fight_style_sufix + "'/>");
+	punch.appendTo('#fighter0');
+	punch.css("left",fight_punchStart).css("top",start_top).css("opacity","0.0");
+	punch.animate({opacity: 1}, {queue: false, duration: 200})
+		 .animate({left: fight_punchEnd, top: end_top}, {queue: false, duration: 400})
+		 .animate({opacity: 1}, 300)
+		 .animate({opacity: 0.0}, 100, callback);
+}
